28b1daaf51d7d53f42ee7b9747430ac39e4773d2
SungKwon <tjdrnjs4759@gmail.com>
2023-07-09 00:52:56 +0900

    first commit

--- Study1/study08.java
+++ Study1/study08.java
@@ -2,7 +2,11 @@ package Study1;
 
 public class study08 {
     public static void main(String[] args) {
-        
+        int a = 10;
+        int b = 6;
+        int result = a - b;
+
+        System.out.println(result);
     }
     
 }
