ca1868d6f4c1b80133ba07e74fd96dbe4a8cab44
Sung_Kwon <tjdrnjs4759@naver.com>
2023-07-09 00:34:36 +0900

    first commit

--- Study1/study03.java
+++ Study1/study03.java
@@ -1,11 +1,9 @@
-//  조성권
 package Study1;
 
 import java.util.Scanner;
 
 public class study03 {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
 
 
---
+++ study/study01.java
@@ -0,0 +1,5 @@
+public class study01 {
+    public static void main(String[] args) {
+
+    }
+}
